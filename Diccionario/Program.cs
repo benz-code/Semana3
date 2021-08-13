67769ccab48c25228d9afa54472d6969507b44db
Eliezer Benjamin Diaz Segovia <diazeliezer19@gmail.com>
2021-08-13 15:26:34 -0600

    Agregar archivos de proyecto.

---
+++ Diccionario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diccionario de Nombres y Edades
+            Dictionary<String, String> NyE = new Dictionary<string, String>();
+
+            NyE.Add("Eliezer Diaz", "18");
+            NyE.Add("Joel Diaz", "20");
+            NyE.Add("Lalo Diaz", "7");
+            NyE.Add("Julio Diaz", "47");
+
+            foreach (KeyValuePair<String, String> E in NyE)
+            {
+                Console.WriteLine("clave: {0}, valor: {1}", E.Key, E.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Semana3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_3_15
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {   //Array de valores ingresados por el usuario
+            int i = 0;
+            int suma = 0;
+            int[] numbers = new int[10];
+            for (i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Escriba el digito {i}");
+                numbers[i] = Int32.Parse(Console.ReadLine());
+                suma += numbers[i];
+            }
+            //Promedio de valores ingresados por el usuario
+           
+            Console.WriteLine($"El promedio es {suma / numbers.Length}");
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
